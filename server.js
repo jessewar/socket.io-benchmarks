93c060bdab7753b4bcad1cf69d0724f606cb570a
Jesse Warren <jesse.warren@live.com>
2014-05-15 18:54:59 -0700

    run with 'new' option doesn't run

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var server = require('http').Server();
 var ioc = require('socket.io-client');
 var Benchmark = require('benchmark');
-var io = require('./node_modules/socket-new/socket.io').listen(server);
+var io;
 
+var version;
+if (process.argv[2] == 'new') {
+    io = require('./node_modules/socket-new/socket.io')(server, {
+        transports : [process.argv[3]]
+    });
+    version = "1.0.0";
+} else {
+    io = require('./node_modules/socket-old/socket.io').listen(server);
+    io.configure(function() {
+	var transport = process.argv.length >= 3 ? process.argv[3] : null;
+	if (transport) io.set('transports', [transport]);
+	io.set('log level', 1);
+    });
+    version = "0.9.16";
+}
 // creates a socket.io client for the given server
 function client(srv, nsp, opts) {
     if ('object' == typeof nsp) {
@@ -15,11 +30,6 @@ function client(srv, nsp, opts) {
     return ioc.connect(url, opts);
 }
 
-io.configure(function() {
-    var transport = process.argv.length >= 3 ? process.argv[3] : null;
-    if (transport) io.set('transports', [transport]);
-    io.set('log level', 1);
-});
 
 io.sockets.on('connection', function(socket) {
     function jsonRoundtrip(deferred) {
@@ -31,15 +41,15 @@ io.sockets.on('connection', function(socket) {
     }
 
     var options = {
-        'onStart' : function() { console.log("Testing SocketIO v0.9.16 using " + process.argv[3] + "..."); },
-        'onComplete' : function() {
+        onStart : function() { console.log("Testing SocketIO v" + version + "  using " + process.argv[3] + "..."); },
+        onComplete : function() {
             console.log("Number of times test was executed: " + this.count + "\n" +
-			"Numer of test cycles: " + this.cycles + "\n" + 
-			"Mean execution time: " + this.stats.mean + "\n" +
-			"Numer of executions per second: " + this.hz);
+                        "Numer of test cycles: " + this.cycles + "\n" +
+                        "Mean execution time: " + this.stats.mean + "\n" +
+                        "Numer of executions per second: " + this.hz);
         },
-        'defer' : true,
-        'async' : true
+        defer : true,
+        async : true
     };
     var bench = new Benchmark('roundtrip', jsonRoundtrip, options);
     bench.run();
