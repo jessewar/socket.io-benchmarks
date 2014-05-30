1a6849e042503a285bb48c8876a034c2bbf72a89
Jesse Warren <jesse.warren@live.com>
2014-05-30 15:07:57 -0700

    separated v0.9.16 and v1.0.0 tests into seperate modules, added package.json

--- bench.js
+++ bench.js
@@ -1,24 +1,9 @@
-var server = require('http').Server();
-var Benchmark = require('benchmark');
-var ioc;
-var io;
+var bench;
 
-var version;
 if (process.argv[2] == '1.0.0') {
-  ioc = require('./node_modules/socket-new/socket.io-client');
-  io = require('./node_modules/socket-new/socket.io')(server, {
-    transports: ['polling', process.argv[3]] // need polling for initial transport
-  });
-  version = "1.0.0";
+  bench = require('socket.io-benchmarks-v1.0.0');
 } else if (process.argv[2] == '0.9.16') {
-  ioc = require('socket.io-client');
-  io = require('socket.io').listen(server);
-  io.configure(function () {
-    var transport = process.argv.length >= 3 ? process.argv[3] : null;
-    if (transport) io.set('transports', [transport]);
-    io.set('log level', 1);
-  });
-  version = "0.9.16";
+  bench = require('socket.io-benchmarks-v0.9.16');
 } else {
   console.log("Usage ./bench.js version transport");
   console.log("Where:");
@@ -27,49 +12,4 @@ if (process.argv[2] == '1.0.0') {
   process.exit(1);
 }
 
-// creates a socket.io client for the given server
-function client(srv, nsp, opts) {
-  if ('object' == typeof nsp) {
-    opts = nsp;
-    nsp = null;
-  }
-  var addr = srv.address();
-  if (!addr) addr = srv.listen().address();
-  var url = 'ws://' + addr.address + ':' + addr.port + (nsp || '');
-  return ioc.connect(url, opts);
-}
-
-
-io.sockets.on('connection', function (socket) {
-  function jsonRoundtrip(deferred) {
-    socket.emit('server-message', {
-      name: "Server"
-    });
-    socket.on('client-message', function (data) {
-      deferred.resolve();
-      socket.removeAllListeners('client-message');
-    });
-  }
-
-  var options = {
-    onStart: function () {
-      console.log("Testing SocketIO v" + version + " using " + process.argv[3] + "...");
-    },
-    onComplete: function () {
-      console.log("Mean time for JSON message from server to client back to server: " + this.stats.mean + "\n" +
-                  "Number of trips per second: " + this.hz);
-      process.exit();
-    },
-    defer: true,
-    async: true
-  };
-  var bench = new Benchmark('roundtrip', jsonRoundtrip, options);
-  bench.run();
-});
-
-
-// connect to server triggering benchmark to start
-var clientSocket = client(server);
-clientSocket.on('server-message', function (data) {
-  clientSocket.emit('client-message', data);
-})
+bench.run(process.argv[3]);
